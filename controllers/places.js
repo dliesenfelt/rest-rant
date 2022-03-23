77410738203cfe3b2103f02da81aa869177cb2e4
dliesenfelt <liesenfelt.code@gmail.com>
2022-03-22 21:19:56 -0700

    updsted resturant

--- controllers/places.js
+++ controllers/places.js
@@ -3,17 +3,23 @@ const router = require('express').Router()
 // GET /places
 router.get('/', (req, res) => {
     let places = [{
-        name: 'Burgers & Beer',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Burgers, Beer',
-        pic: '/images/burger.png'
+        name: 'La Granja',
+        city: 'Margate',
+        state: 'FL',
+        cuisines: 'Peruvian',
+        pic: 'https://www.lagranjarestaurants.com/media/uploads/ofertas/promo-1-home.png?a=1'
     }, {
-        name: 'Tacomania',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Mexican',
-        pic: '/images/tacos.jpeg'
+        name: 'Mike Hess Brewing',
+        city: 'Imperial Beach',
+        state: 'CA',
+        cuisines: 'Tacos, Beer',
+        pic: 'https://www.mikehessbrewing.com/wp-content/uploads/2021/06/SteelBeach_IPA_MikeHess_2021_BeerCan.png'
+    }, {
+        name: 'Tajima',
+        city: 'San Diego',
+        state: 'CA',
+        cuisines: 'Japanese, Ramen',
+        pic: 'https://tajimasandiego.com/wp-content/uploads/layerslider/Slider-1/Vegan-Tajima-Ramen-1-1024x684.jpg'
     }]
     res.render('places/index', { places })
 })

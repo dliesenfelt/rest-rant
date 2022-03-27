a546349a823fb905ecaf3c062f63d5186be84ca9
dliesenfelt <liesenfelt.code@gmail.com>
2022-03-26 23:38:59 -0700

    part 3 complete

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,23 @@
 const router = require('express').Router()
 
+//GET places
 router.get('/', (req, res) => {
-    res.send('GET /places')
+    let places = [{
+        name: 'Tajima',
+        city: 'San Diego',
+        state: 'CA',
+        cuisines: 'Ramen, Japaneese',
+        pic: 'https://i.ytimg.com/vi/VHkkKbedGsU/maxresdefault.jpg',
+    }, 
+    {
+        name: 'La Granja',
+        city: 'Margate',
+        state: 'FL',
+        cuisines: 'Peruvian',
+        pic: 'https://www.lagranjarestaurants.com/media/uploads/ofertas/promo-2-home.png?a=1',
+    },
+]
+    res.render('places/index', {places})
 })
 
 module.exports = router

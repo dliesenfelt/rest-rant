0c67d89f2431a381effd749c566a0c8a0c280f41
dliesenfelt <liesenfelt.code@gmail.com>
2022-03-27 02:07:44 -0700

    part 7 complete

--- controllers/places.js
+++ controllers/places.js
@@ -55,7 +55,33 @@ router.get('/:id', (req, res) => {
       res.render('places/show', {place: places[id], id})
     }
 });
-//GET places/:id/edit
 
+//GET places/:id/edit
+router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+      res.render('places/edit', { place: places[id] })
+    }
+});
+
+//PUT places/:id/edit
+router.put('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        res.redirect(`/places/${id}`)
+    }
+  })
 
 module.exports = router
